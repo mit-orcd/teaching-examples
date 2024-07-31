6dd1a71f98b58858dec11f326139a7420a6a6763
kcnakamu <aquachat77@gmail.com>
2024-07-31 10:13:19 -0400

    Fix Parallel Directory Path

--- .test/Python/word_count/Parallel/submit_SPMD.sh
+++ .test/Python/word_count/Parallel/submit_SPMD.sh
@@ -10,5 +10,5 @@ module load gcc/12.2.0-x86_64
 module load openmpi/4.1.4-pmi-cuda-ucx-x86_64
 
 # Call your script as you would from the command line
-cd ../../../../Python/word_count/Parallel
+cd ../../../../Python/word_count/Parallel/mpi4py
 mpirun python top5norm_SPMD.py
