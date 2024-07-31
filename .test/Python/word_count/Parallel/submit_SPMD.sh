7103d4e71ab39709c13c8bebf36764b24a1f1f4b
kcnakamu <aquachat77@gmail.com>
2024-07-31 10:02:16 -0400

    Add Python Scripts

---
+++ .test/Python/word_count/Parallel/submit_SPMD.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Slurm sbatch options
+#SBATCH -o top5norm_SPMD.log-%j
+#SBATCH -n 4
+
+# Load Anaconda and MPI module
+module load anaconda3/2022.05-x86_64   
+module load gcc/12.2.0-x86_64  
+module load openmpi/4.1.4-pmi-cuda-ucx-x86_64
+
+# Call your script as you would from the command line
+cd ../../../../Python/word_count/Parallel
+mpirun python top5norm_SPMD.py
---
+++ .test/Python/word_count/Serial/submit_each.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Load Anaconda Module
+module load anaconda3/2022.05-x86_64 
+
+# Call your script as you would from the command line
+cd ../../../../Python/word_count/Serial
+python top5each.py
