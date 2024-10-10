b5cc343d25600f7575ecd9e20a292ee83861cec7
kcnakamu <aquachat77@gmail.com>
2024-10-10 11:26:07 -0400

    Update test

--- .test/Julia/word_count/serial/submit_each.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load Julia Module
-module load julia/1.9.1
-
-# Call your script as you would from the command line
-cd ../../../../Julia/word_count/Serial
-julia top5each.jl
--- .test/Python/word_count/Serial/submit_overall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load Anaconda Module
-module load anaconda3/2022.05-x86_64 
-
-# Call your script as you would from the command line
-cd ../../../../Python/word_count/Serial
-python top5overall.py
